670402d5fc217bbb028c73bb0ef3701584a2829a
yuki <yuki@yuki-mac.local>
2021-04-17 15:51:59 +0900

    devise実装中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root "home#index"
+  
+  devise_for :users => {
+    :sessions => "users/sessions"
+  }
+  
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
