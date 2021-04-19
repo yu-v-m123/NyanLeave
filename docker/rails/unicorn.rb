c3209339df684b9f2ba8304f1d62b8d3936de43c
yu <nagase.yuki0123@gmail.com>
2021-04-20 00:39:22 +0900

    コンテナ内のログ出力設定

--- docker/rails/unicorn.rb
+++ docker/rails/unicorn.rb
@@ -3,5 +3,7 @@ worker_processes 8
 pid "/var/run/unicorn.pid"
 listen "/var/tmp/unicorn.sock"
 
-stdout_path "./log/unicorn.stdout.log"
-stderr_path "./log/unicorn.stderr.log"
+# stdout_path "./log/unicorn.stdout.log"
+# stderr_path "./log/unicorn.stderr.log"
+stdout_path = $stderr
+stderr_path = $stdout
