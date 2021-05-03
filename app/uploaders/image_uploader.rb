dce109d9d9fcda5a114a445d7508ffa09e395b61
yu <nagase.yuki0123@gmail.com>
2021-05-03 18:15:46 +0900

    miniサイズ作成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,6 +35,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [160, 160]
   end
 
+  version :mini do
+    process resize_to_fit: [45, 45]
+  end
+
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
