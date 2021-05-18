243bcce9740ffa7ebc6bb769453d277a8f5ebcfd
yu <nagase.yuki0123@gmail.com>
2021-05-18 22:20:22 +0900

    generate時のcssとjsファイル生成拒否

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
