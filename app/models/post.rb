fab044bc99156a0bdba589d4af0c40a2d3dac308
yu <nagase.yuki0123@gmail.com>
2021-05-21 07:08:20 +0900

    Profile、Postモデル内での各入力ページのバリデーション実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,25 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :profile
+
+  with_options presence: true do
+    validates :start
+    validates :finish
+    validates :place
+    with_options length: { maximum:500 } do
+      validates :feature
+    end
+  end
+
+  validate :date_before_start
+  validate :date_after_finish
+  def date_before_start
+    return if start.blank?
+      errors.add(:start, "は今日以降のものを選択してにゃ") if start < Date.today
+  end
+  
+  def date_after_finish
+    return if finish.blank? || start.blank?
+      errors.add(:finish, "は開始日以降のものを選択してにゃ") if finish < start
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,17 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :posts
   mount_uploader :image, ImageUploader
+
+  with_options presence: true do
+    validates :image
+    validates :address
+    with_options length: { maximum:7 } do
+      validates :user_name
+      validates :cat_name
+      validates :age
+      end
+    with_options length: { maximum:500 } do
+      validates :introduction
+    end
+  end
 end
