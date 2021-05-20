8354312e1989dc9e116e9236759bab5af90a7e50
yu <nagase.yuki0123@gmail.com>
2021-05-21 07:02:55 +0900

    終了日のカラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_123007) do
+ActiveRecord::Schema.define(version: 2021_05_20_204117) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "start"
-    t.date "end"
+    t.date "finish"
     t.string "place"
     t.text "feature"
     t.bigint "user_id"
