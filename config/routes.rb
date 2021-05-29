fb4bc8b3bfa7d4993762d9118e90902f2c099bf9
yu <nagase.yuki0123@gmail.com>
2021-05-29 22:50:21 +0900

    いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
   
   resources :users, only: [:new, :create, :show]
   resources :profiles, only: [:new, :create, :edit, :update]
-  resources :posts
+  resources :posts do
+    resource :like, only: [:create, :destroy]
+  end
   resources :cares, only: [:index, :show]
   get "cares/owner/:id", to: "cares#owner", as: "care_owner"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_204117) do
+ActiveRecord::Schema.define(version: 2021_05_29_082703) do
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "start"
@@ -50,6 +59,8 @@ ActiveRecord::Schema.define(version: 2021_05_20_204117) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "profiles"
   add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
