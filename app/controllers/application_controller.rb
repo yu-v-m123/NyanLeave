3b3c702ee4f64eb80f8469069462a0faa0a59d18
yu <nagase.yuki0123@gmail.com>
2021-06-03 03:37:53 +0900

    perメソッド指定 #17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ before_action :set_search
 
   def set_search
       @search = Post.ransack(params[:q])
-      @search_posts = @search.result(distinct: true).page(params[:page]).per(4)
+      @search_posts = @search.result(distinct: true).page(params[:page]).per(12)
       @search_count = @search.result.count
   end
 end
