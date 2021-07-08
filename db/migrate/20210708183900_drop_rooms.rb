866deb1769484f966a8808f56db5f2dca605a92b
yu <nagase.yuki0123@gmail.com>
2021-07-09 04:13:23 +0900

    create migration file

---
+++ db/migrate/20210708183900_drop_rooms.rb
@@ -0,0 +1,5 @@
+class DropRooms < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :rooms
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_082703) do
+ActiveRecord::Schema.define(version: 2021_07_08_185211) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "post_id"
